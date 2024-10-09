ab1484256b760885cf0471531d82f9daecbf6201
flodro34 <f.derosso@gmail.com>
2024-10-09 15:07:20 +0200

    first commit

---
+++ src/main/java/fr/fdr/projetjo/ProjetJoApplication.java
@@ -0,0 +1,13 @@
+package fr.fdr.projetjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetJoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/fdr/projetjo/ProjetJoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fdr.projetjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
